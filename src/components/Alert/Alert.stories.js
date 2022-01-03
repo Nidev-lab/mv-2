603126a070370d2d6a7cd9bd1cc242feb13aac45
Nicolas Gonzalez <ngonzalez@dminc.com>
2022-01-03 17:09:05 -0300

    proyect mv-2

---
+++ src/components/Alert/Alert.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Alert from './Alert';
+
+export default {
+  title: 'MV-2/Alert',
+  component: Alert,
+};
+
+const Template = () => <Alert />;
+
+export const Default = Template.bind({});
